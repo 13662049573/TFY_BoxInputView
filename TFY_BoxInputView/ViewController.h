5b7f92890d726c3853935466ac1dd5500c760f71
tfyzxc13662049573 <420144542@qq.com>
2020-06-16 13:39:48 +0800

    Initial Commit

---
+++ TFY_BoxInputView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TFY_BoxInputView
+//
+//  Created by tiandengyou on 2020/6/16.
+//  Copyright © 2020 田风有. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ TFY_BoxInputView/main.m
@@ -0,0 +1,19 @@
+//
+//  main.m
+//  TFY_BoxInputView
+//
+//  Created by tiandengyou on 2020/6/16.
+//  Copyright © 2020 田风有. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    NSString * appDelegateClassName;
+    @autoreleasepool {
+        // Setup code that might create autoreleased objects goes here.
+        appDelegateClassName = NSStringFromClass([AppDelegate class]);
+    }
+    return UIApplicationMain(argc, argv, nil, appDelegateClassName);
+}

525f1eb0b58bee0fe28a7fdc6ede79bdc767ad86
420144542@qq.com <420144542@qq.com>
2020-09-10 15:36:03 +0800

    优化

---
+++ TFY_BoxInputView/TFY_BoxInputITools/TFY_BoxInputITools.h
@@ -0,0 +1,27 @@
+//
+//  TFY_BoxInputITools.h
+//  TFY_BoxInputView
+//
+//  Created by 田风有 on 2020/9/10.
+//  Copyright © 2020 田风有. All rights reserved.
+//  最新版本 : 2.0.8
+
+#import <Foundation/Foundation.h>
+
+FOUNDATION_EXPORT double TFY_BoxInputIToolsVersionNumber;
+
+FOUNDATION_EXPORT const unsigned char TFY_BoxInputIToolsVersionString[];
+
+#define TFY_BoxInputIToolsRelease 0
+
+#if TFY_BoxInputIToolsRelease
+
+#import <TFY_BoxInputITools/TFY_LineView.h>
+#import <TFY_BoxInputITools/TFY_BoxInputView.h>
+
+#else
+
+#import "TFY_LineView.h"
+#import "TFY_BoxInputView.h"
+
+#endif
